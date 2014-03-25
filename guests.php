b07a11837bed793b3d8b51b971dba9667f6e1129
Callie Kavourgias <calliemariek@gmail.com>
2014-03-24 22:30:32 -0400

    Spacing fixed: Guests

--- guests.php
+++ guests.php
@@ -11,32 +11,36 @@ $view->showHeader();
 ?>
 <div id="guests">
 	<div id="okc">
+	<img class="right" src="img/okc.jpg" alt="Oklahoma City" width="276" height="185"/>
 		<h2>Oklahoma City</h2>
 		<p>Oklahoma City has grown considerably in the last 20 years.  There is so much to do around the metro area.</p> 
 		<p>You could check out downtown, known as Bricktown has many options for dining and
-entertainment.  Bricktown also has a beautiful riverwalk to check out.  The wedding location is near Quail Springs Mall, which hosts an AMC 24. There are also a lot of other shopping opportunities.</p>
-		<img src="img/okc.jpg" alt="Oklahoma City" width="276" height="185"/>
+entertainment. Bricktown also has a beautiful riverwalk to check out.  The wedding location is near Quail Springs Mall, which hosts an AMC 24. There are also a lot of other shopping opportunities.</p>
+		
 	</div>
 	
 	<div id="accomodations">
 	<h2>Accomodations</h2>
 		<div id="location1">
-			<h5><a href="#">Springhill Suites</a></h5>
-			<img src="img/springhill.jpg" alt="Springhill Suites" width="207" height="127"/>
+		<img class="right" src="img/springhill.jpg" alt="Springhill Suites" width="207" height="127"/>
+			<h5><a href="http://www.marriott.com/hotels/travel/okcsh-springhill-suites-oklahoma-city-quail-springs/">Springhill Suites</a></h5>
 			<p>123 Main</p>
+			<p>OKC, OK 73071</p>
 		</div>
+		
 		<div id="location2">
-			<h5><a href="#">Courtyard Marriott</a></h5>
-			<img src="img/courtyard.jpg" alt="Courtyard Marriott" width="207" height="127"/>
+		<img src="img/courtyard.jpg" alt="Courtyard Marriott" width="207" height="127"/>
+			<h5><a href="http://www.marriott.com/hotels/travel/okcdt-courtyard-oklahoma-city-downtown/">Courtyard Marriott</a></h5>
+			
 			<p>433 Main</p>
+			<p>Edmond, OK 73013</p>
 		</div>	
+		
 	</div>
 	
 	<div id="registries">
 		<h2>Registries</h2>
-		<p>Callie and Grant are registered at the locations below. Please click the photo to take you to 		each specific registry.</p>
-		<a class="reg" href="http://wwww.kohls.com"><img src="img/kohls.jpg" alt="Kohls" width="300" height="130"/></a>
-		<a class="reg" href="http://wwww.williams-sonoma.com"><img src="img/williamsonoma-1.jpg" alt="William Sonoma" width="355" height="130"/></a>
+		<a href="http://wwww.kohls.com"><img class="reg" src="img/kohls.jpg" alt="Kohls" width="300" height="130"/></a>
+		<a href="http://wwww.williams-sonoma.com"><img class="reg" src="img/williamsonoma-1.jpg" alt="William Sonoma" width="350" height="130"/></a>
 	</div>
-</div>	
 </body>
